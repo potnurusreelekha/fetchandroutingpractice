5c95fd75f1cfa4a8c28e10f01bb39b090df9ec21
potnurusreelekha <157681654+potnurusreelekha@users.noreply.github.com>
2024-05-07 11:50:27 +0530

    Update index.js

--- src/components/BlogList/index.js
+++ src/components/BlogList/index.js
@@ -1,5 +1,5 @@
 // Write your JS code here
-import Component from 'react'
+import {Component} from 'react'
 
 import Loader from 'react-loader-spinner'
 import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
